22c5bebe6996febacaa713c55ec94665b2139061
edekr <ekrotter@mailbox.org>
2022-08-09 14:49:30 +0200

    lambda fix 3

--- src/build_source_code.sh
+++ src/build_source_code.sh
@@ -2,5 +2,5 @@ mkdir ../infrastructure/build
 
 cd fetch_image_data_lambda
 
-pip install -r requirements.txt -t python/lib/python3.9/site-packages
+pip install -r requirements.txt -t fetch_image_data_lambda
 zip -r ../../infrastructure/build/fetch_image_data_lambda.zip .
