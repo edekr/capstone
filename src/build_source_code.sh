e3a787f000467165ca2f53ab01bd597f705d7dbe
edekr <ekrotter@mailbox.org>
2022-08-09 14:25:37 +0200

    fix lambda function

--- src/build_source_code.sh
+++ src/build_source_code.sh
@@ -1,6 +1,6 @@
 mkdir ../infrastructure/build
 
 cd fetch_image_data_lambda
-pip install exif
+
 pip install -r requirements.txt -t python/lib/python3.9/site-packages
 zip -r ../../infrastructure/build/fetch_image_data_lambda.zip .
