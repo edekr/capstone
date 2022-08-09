e7698ab65a12268de75901546e272dfc55e25668
edekr <ekrotter@mailbox.org>
2022-08-09 14:30:45 +0200

    lambda fix 2

--- infrastructure/fetch_image_data.tf
+++ infrastructure/fetch_image_data.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "fetch_image_data_lambda" {
   filename      = "build/fetch_image_data_lambda.zip"
   function_name = "fetch_image_data_lambda"
   role          = "arn:aws:iam::018572766339:role/LabRole"
-  handler       = "fetch_image_data_new.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
 
 
   source_code_hash = filebase64sha256("build/fetch_image_data_lambda.zip")
