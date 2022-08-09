4e5b5bed296239b9f34684254713cacd55e1ed8d
edekr <ekrotter@mailbox.org>
2022-08-09 16:30:19 +0200

    update package

--- src/build_source_code.sh
+++ src/build_source_code.sh
@@ -2,5 +2,5 @@ mkdir ../infrastructure/build
 
 cd fetch_image_data_lambda
 
-pip install -r requirements.txt -t fetch_image_data_lambda
+pip install -r requirements.txt -t package
 zip -r ../../infrastructure/build/fetch_image_data_lambda.zip .
